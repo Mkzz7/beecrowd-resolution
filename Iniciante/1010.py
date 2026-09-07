0bf0cc6fc036eee562298fecf2590ce3c0422499
mikael <mikezyt7@gmail.com>
2026-09-07 15:08:51 -0300

    resolução do exercício 1010

---
+++ Iniciante/1010.py
@@ -0,0 +1,9 @@
+code1 = int(input(""))
+units1 = int(input(""))
+price1 = float(input("")) 
+
+code2 = int(input(""))
+units2 = int(input(""))
+price2 = float(input("")) 
+
+print(f"VALOR A PAGAR: R$ {(units1 * price1) + (units2 * price2):.2f}")
