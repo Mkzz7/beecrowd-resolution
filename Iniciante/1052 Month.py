778b7a9786c497112a81087c8e29f28e1e5a6880
mikael <mikezyt7@gmail.com>
2026-09-23 10:28:12 -0300

    R.D.E 1052

---
+++ Iniciante/1052 Month.py
@@ -0,0 +1,13 @@
+months = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+    ]
+
+month = (int(input("")))
+
+if 1 <= month <= 12:
+    print(months[month - 1])
+else:
+    print("Invalid month")
